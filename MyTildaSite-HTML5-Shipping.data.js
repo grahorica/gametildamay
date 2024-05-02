f2849c6e026453bd28c822aa063f65633696d2f0
grahorica <134962063+grahorica@users.noreply.github.com>
2024-05-02 21:36:44 +0300

    Update MyTildaSite-HTML5-Shipping.data.js

--- MyTildaSite-HTML5-Shipping.data.js
+++ MyTildaSite-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'D:/MyTildaSite/Binaries/HTML5/MyTildaSite-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyTildaSite-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/dkqqt1qk857p7rsx5bhot/MyTildaSite-HTML5-Shipping.data?rlkey=0sosbyku625o8ior69z487wg0&st=1elusk74&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/dkqqt1qk857p7rsx5bhot/MyTildaSite-HTML5-Shipping.data?rlkey=0sosbyku625o8ior69z487wg0&st=1elusk74&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
